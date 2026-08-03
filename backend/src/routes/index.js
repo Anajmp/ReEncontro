87211da6cd06a583f7b6be1eebd49331bf29e025
I2HN <anajmp11@gmail.com>
2026-08-03 15:08:28 -0300

    chore: remove rota de setup

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -9,13 +9,10 @@ import authRoutes from "./authRoutes.js";
 // import usuariosRoutes from './usuariosRoutes.js';
 // import relatoriosRoutes from './relatoriosRoutes.js';
 
-import setupRoute from "./setupRoute.js";
-
 const router = Router();
 
 router.use("/itens", itensRoutes);
 router.use("/auth", authRoutes);
-router.use("/", setupRoute);
 // router.use('/reivindicacoes', reivindicacoesRoutes);
 // router.use('/usuarios', usuariosRoutes);
 // router.use('/relatorios', relatoriosRoutes);
